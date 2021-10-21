d70f65a644566284f314b3d245e3c143f5e9cfca
afc2134 <85919290+AFC2134@users.noreply.github.com>
2021-10-21 00:51:47 -0500

    employee test set up, not working

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,8 @@
+const Employee = require('../lib/Employee');
+
+test('creates a Employee object', () => {
+    const Employee = new Employee('Alex', '1', "alexC@gmail.com")
+    expect(alex.name).toEqual(expect.any(String));
+    expect(alex.id).toEqual(expect.any(String));
+    expect(alex.email).toEqual(expect.any(String));
+})
