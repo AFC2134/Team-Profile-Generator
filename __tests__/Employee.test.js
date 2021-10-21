336e696774b5f060518e59ec591a10ac81a6f7a5
afc2134 <85919290+AFC2134@users.noreply.github.com>
2021-10-21 01:08:40 -0500

    fix employee test contructor

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,7 +2,7 @@ const { test } = require('@jest/globals')
 const Employee = require('../lib/Employee');
 
 test('creates a Employee object', () => {
-    const Employee = new Employee('Alex', '1', "alexC@gmail.com")
+    const alex = new Employee('Alex', '1', "alexC@gmail.com")
     expect(alex.name).toEqual(expect.any(String));
     expect(alex.id).toEqual(expect.any(String));
     expect(alex.email).toEqual(expect.any(String));
