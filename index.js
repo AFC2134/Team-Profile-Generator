e204c34e895a8d26e2fe5177c42cef34a456d676
afc2134 <85919290+AFC2134@users.noreply.github.com>
2021-10-20 23:57:24 -0500

    inquirer prompts in index.js functional

--- index.js
+++ index.js
@@ -6,10 +6,25 @@ function questions() {
     .prompt([
         {
         type: 'list',
-        name: 'emplyeeClass',
+        name: 'employeeClass',
         message: 'What type of employee are you?',
         choices: ['manager','engineer', 'intern']
+        },
+        {
+        type: 'input',
+        name: 'employeeName',
+        message: 'What is your Name?',
+        },
+        {
+            type: 'input',
+            name: 'employeeID',
+            message: 'What is your Employee ID?',
+        },
+        {
+            type: 'input',
+            name: 'employeeEmail',
+            message: 'What is your employee Email'
         }
     ])
-}
+};
 questions()
