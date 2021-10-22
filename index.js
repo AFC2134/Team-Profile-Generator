e714dce2917701e6addae54be7e3d83a58d62d1b
afc2134 <85919290+AFC2134@users.noreply.github.com>
2021-10-22 00:10:03 -0500

    inquier prompts, gen markdown promise handle, manager actions, refactor project, create working employee constructor

--- index.js
+++ index.js
@@ -1,30 +1,87 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+let allEmployees = [];
 
 function questions() {
+
+
+    // inquirer
+    // .prompt([
+    //     {
+    //     type: 'input',
+    //     name: 'managerName',
+    //     message: "what is the name of this manager?",
+    //     message: "what is the employee ID of this manager?",
+    //     messege: "what is the"
+    // }
+    // ])
+
+
+
+
+
+
+
     inquirer
-    .prompt([
-        {
-        type: 'list',
-        name: 'employeeClass',
-        message: 'What type of employee are you?',
-        choices: ['manager','engineer', 'intern']
-        },
-        {
-        type: 'input',
-        name: 'employeeName',
-        message: 'What is your Name?',
-        },
-        {
-            type: 'input',
-            name: 'employeeID',
-            message: 'What is your Employee ID?',
-        },
-        {
-            type: 'input',
-            name: 'employeeEmail',
-            message: 'What is your employee Email'
-        }
-    ])
-};
+        .prompt([
+            {
+                type: 'list',
+                name: 'employeeClass',
+                message: 'What type of employee are you?',
+                choices: ['manager', 'engineer', 'intern']
+            },
+            {
+                type: 'input',
+                name: 'employeeName',
+                message: "What is the employee's Name?",
+            },
+            {
+                type: 'input',
+                name: 'employeeId',
+                message: 'What is the Employee ID?',
+            },
+            {
+                type: 'input',
+                name: 'employeeEmail',
+                message: 'What is the employee Email'
+            }
+        ])
+        .then(empData => {
+            if (empData.employeeClass === "manager") {
+                inquirer
+                    .prompt([
+                        {
+                            type: "input",
+                            name: "officeNum",
+                            message: "What is your office number?"
+                        },
+                        {
+                            type: "list",
+                            name: "updateEmp",
+                            message: "Would you like to add another employee or Complete Team?",
+                            choices: ["Add Employee", "Complete Team"]
+                        }
+
+                    ])
+
+                    .then(managerAnswers => {
+                        let managerAction = new Manager(empData.employeeName, empData.employeeId, empData.employeeEmail, managerAnswers.officeNum)
+                        allEmployees.push(managerAction)
+                        if (managerAnswers.updateEmp === "Add Employee") {
+                            questions();
+                        } else {
+                            let HTML = generateMarkdown(allEmployees)
+                            fs.writeFile("index", HTML , err)
+                        }
+
+                    })
+            }
+        // else if if users answers engineer , then ask questions that pretain to engineer, intern ect. push new data to new array , else generatenardown. repeate adding to Array.. make new classes for enginner and intern , use super, specific to them info same as manager constructor... console log to trial error template literal, 
+        })
+
+
+
+}
+
 questions()
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ function generateMarkdown() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
     </head>
-    <body>
+    <body>$
         
     </body>
     </html>
